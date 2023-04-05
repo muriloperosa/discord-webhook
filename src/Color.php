5c03bb9d30d27a1e637c03ba64b0312edf9c7162
Fernando Jr <jr.fernandodasilva@gmail.com>
2023-04-05 15:08:59 -0300

    style: code formatting

--- src/Color.php
+++ src/Color.php
@@ -4,9 +4,9 @@ namespace PhpChannels\DiscordWebhook;
 
 class Color 
 {
-    const LOGGER_INFO     = 2450411;
-    const LOGGER_ERROR    = 14427686;
-    const LOGGER_NOTICE   = 4937059;
-    const LOGGER_WARNING  = 14251782;
-    const LOGERR_SUCCESS  = 366185;
+    const LOGGER_INFO    = 2450411;
+    const LOGGER_ERROR   = 14427686;
+    const LOGGER_NOTICE  = 4937059;
+    const LOGGER_WARNING = 14251782;
+    const LOGERR_SUCCESS = 366185;
 }
